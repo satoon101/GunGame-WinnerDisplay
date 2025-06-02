3688fdd4f8aa664153e5794be18c99de65fa8541
satoon101 <satoon101@gmail.com>
2025-06-02 09:37:37 -0400

    Updated convars to use built-ins.

--- addons/source-python/plugins/gungame/plugins/custom/gg_winner_display/gg_winner_display.py
+++ addons/source-python/plugins/gungame/plugins/custom/gg_winner_display/gg_winner_display.py
@@ -65,5 +65,5 @@ def _send_motd(**kwargs):
     """Send the winner display page."""
     message_manager.motd_message(
         title="GunGame Winner",
-        message=f"{winner_page.get_string()}?{urlencode(kwargs)}",
+        message=f"{str(winner_page)}?{urlencode(kwargs)}",
     )
